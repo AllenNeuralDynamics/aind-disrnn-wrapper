9f031a9d7db9e0455ed730ebde3f45f7d6a08c98
houhan@gmail.com <han.hou@alleninstitute.org>
2025-12-01 06:33:26 +0000

    add extra tags

--- code/utils/run_helpers.py
+++ code/utils/run_helpers.py
@@ -59,13 +59,18 @@ def start_wandb_run(
     hydra_config: DictConfig,
 ) -> Optional[wandb.sdk.wandb_run.Run]:
     dict_config = OmegaConf.to_container(hydra_config, resolve=True)
+
+    # Extract & remove original tags
+    base_tags = wandb_cfg.pop("tags", []) or []
+    extra_tags = [hydra_config.data.type, hydra_config.model.type]
+
+    # Init wandb with merged tags
     run = wandb.init(
-        **dict_config.get("wandb", {}),
+        **wandb_cfg,
         config={k: dict_config[k] for k in ("data", "model") if k in dict_config},
-        tags=[hydra_config.data.type, hydra_config.model.type],
+        tags=base_tags + extra_tags,
     )
     
     # System environment variable for CO 
-    # TODO: this only works for capsule run at this point
     run.config.update({"CO_COMPUTATION_ID": os.environ.get("CO_COMPUTATION_ID")})
     return run
