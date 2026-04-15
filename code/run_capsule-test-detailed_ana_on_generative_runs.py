66ce8c48a4c38c84c280cb08e2f4fb464d1f6b9b
pckuo <pckuo@uw.edu>
2026-04-15 00:57:50 +0000

    Edited run; added run_capsule-test-detailed_ana_on_generative_runs.py

---
+++ code/run_capsule-test-detailed_ana_on_generative_runs.py
@@ -0,0 +1,17 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+)
+
+
+from post_training_analysis import run_post_training_analysis_from_saved_histories
+
+result = run_post_training_analysis_from_saved_histories(
+    simulated_session_history_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/simulated_session_history.pkl",
+    resolved_run_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/resolved_run.json",
+    output_dir="/results",
+)
+
+print(result["model_vs_animal_quantitative_summary"])
