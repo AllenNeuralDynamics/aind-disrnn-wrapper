430d91d223da1b474d71599329c5e342f2632b30
pckuo <pckuo@uw.edu>
2026-04-15 05:00:11 +0000

    Edited run_capsule-test-detailed_ana_on_generative_runs.py

--- code/run_capsule-test-detailed_ana_on_generative_runs.py
+++ code/run_capsule-test-detailed_ana_on_generative_runs.py
@@ -8,16 +8,19 @@ logging.basicConfig(
 
 from post_training_analysis import run_post_training_analysis_from_saved_histories
 
+result = run_post_training_analysis_from_saved_histories(
+    simulated_session_history_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/simulated_session_history.pkl",
+    resolved_run_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/resolved_run.json",
+    output_dir="/results",
+    window_size=10,
+    save_animal_session_history=False,
+    session_partitions=("train", "eval", "combined"),
+)
+
 # result = run_post_training_analysis_from_saved_histories(
-#     simulated_session_history_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/simulated_session_history.pkl",
-#     resolved_run_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/resolved_run.json",
+#     simulated_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/QLearning_L1F1_CK1_softmax/simulated_session_history.pkl",
+#     animal_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/animal_session_history.pkl",
 #     output_dir="/results",
 # )
-
-result = run_post_training_analysis_from_saved_histories(
-    simulated_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/QLearning_L1F1_CK1_softmax/simulated_session_history.pkl",
-    animal_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/animal_session_history.pkl",
-    output_dir="/results",
-)
 
 print(result["model_vs_animal_quantitative_summary"])
