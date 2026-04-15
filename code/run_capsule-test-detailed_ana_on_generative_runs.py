b3f08d827b52d512594886f9301860c23225c860
pckuo <pckuo@uw.edu>
2026-04-15 18:40:49 +0000

    Edited run_capsule-test-detailed_ana_on_generative_runs.py

--- code/run_capsule-test-detailed_ana_on_generative_runs.py
+++ code/run_capsule-test-detailed_ana_on_generative_runs.py
@@ -8,15 +8,19 @@ logging.basicConfig(
 
 from post_training_analysis import run_post_training_analysis_from_saved_histories
 
-result = run_post_training_analysis_from_saved_histories(
-    simulated_session_history_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/simulated_session_history.pkl",
-    resolved_run_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/resolved_run.json",
-    output_dir="/results",
-    window_size=10,
-    save_animal_session_history=False,
-    session_partitions=("train", "eval", "combined"),
-)
+# # gru
+# result = run_post_training_analysis_from_saved_histories(
+#     simulated_session_history_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/simulated_session_history.pkl",
+#     resolved_run_path="/data/generative_runs-multisubject_train10-gru-260323-14-H64_lr1e-05-ca41751fa454-5_runs/resolved_run.json",
+#     output_dir="/results",
+#     window_size=10,
+#     save_animal_session_history=False,
+#     session_partitions=("train", "eval", "combined"),
+# )
 
+
+
+# # bari
 # result = run_post_training_analysis_from_saved_histories(
 #     simulated_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/QLearning_L1F1_CK1_softmax/simulated_session_history.pkl",
 #     animal_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/animal_session_history.pkl",
@@ -25,4 +29,13 @@ result = run_post_training_analysis_from_saved_histories(
 #     save_animal_session_history=False,
 # )
 
-print(result["model_vs_animal_quantitative_summary"])
+# hattori
+result = run_post_training_analysis_from_saved_histories(
+    simulated_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/QLearning_L2F1_softmax/simulated_session_history.pkl",
+    animal_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/animal_session_history.pkl",
+    output_dir="/results",
+    window_size=10,
+    save_animal_session_history=False,
+)
+
+# print(result["model_vs_animal_quantitative_summary"])
