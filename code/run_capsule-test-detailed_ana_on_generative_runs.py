8a1658320ab526147ea68ec739deff1499590411
pckuo <pckuo@uw.edu>
2026-05-18 21:24:20 +0000

    Edited run, run_capsule-test-detailed_ana_on_generative_runs.py

--- code/run_capsule-test-detailed_ana_on_generative_runs.py
+++ code/run_capsule-test-detailed_ana_on_generative_runs.py
@@ -29,26 +29,26 @@ from post_training_analysis import run_post_training_analysis_from_saved_histori
 #     session_partitions=("train", "eval", "combined"),
 # )
 
-# gru, no subject_embedding
-result = run_post_training_analysis_from_saved_histories(
-    simulated_session_history_path="/data/generative_runs-train10_test3-gru-260324-14-H64_lr5e-06-574b07ba8c89-5_runs/simulated_session_history.pkl",
-    resolved_run_path="/data/generative_runs-train10_test3-gru-260324-14-H64_lr5e-06-574b07ba8c89-5_runs/resolved_run.json",
-    output_dir="/results",
-    window_size=10,
-    save_animal_session_history=False,
-    session_partitions=("train", "eval", "combined"),
-)
-
-
-
-# # bari
+# # gru, no subject_embedding
 # result = run_post_training_analysis_from_saved_histories(
-#     simulated_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/QLearning_L1F1_CK1_softmax/simulated_session_history.pkl",
-#     animal_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/animal_session_history.pkl",
+#     simulated_session_history_path="/data/generative_runs-train10_test3-gru-260324-14-H64_lr5e-06-574b07ba8c89-5_runs/simulated_session_history.pkl",
+#     resolved_run_path="/data/generative_runs-train10_test3-gru-260324-14-H64_lr5e-06-574b07ba8c89-5_runs/resolved_run.json",
 #     output_dir="/results",
 #     window_size=10,
 #     save_animal_session_history=False,
+#     session_partitions=("train", "eval", "combined"),
 # )
+
+
+
+# bari
+result = run_post_training_analysis_from_saved_histories(
+    simulated_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/QLearning_L1F1_CK1_softmax/simulated_session_history.pkl",
+    animal_session_history_path="/data/generative_runs-multisubject_train10-baseline_rl/animal_session_history.pkl",
+    output_dir="/results",
+    window_size=10,
+    save_animal_session_history=False,
+)
 
 # # hattori
 # result = run_post_training_analysis_from_saved_histories(
